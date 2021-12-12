c5bd8b92118b74d51f04e2bcfc7bb10036a3672e
MatixYo <matixovi@gmail.com>
2021-12-12 01:02:03 +0100

    Find on EC

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { SearchService } from './search.service';
 
 @Controller('search')
 export class SearchController {
+  constructor(private readonly searchService: SearchService) {}
+
   @Get()
-  findAll(): any[] {
-    return ['This action returns all cats'];
+  async findAll(@Query('q') q: string): Promise<any[]> {
+    const response = await this.searchService.findOnEc(q);
+    return response;
   }
 }
--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
-export class SearchService {}
+export class SearchService {
+  constructor(private httpService: HttpService) {}
+
+  async findOnEc(query: string): Promise<any[]> {
+    try {
+      return lastValueFrom(
+        this.httpService
+          .post(
+            'https://api.tech.ec.europa.eu/search-api/prod/rest/search',
+            {},
+            {
+              params: {
+                apiKey: 'SEDIA',
+                text: query,
+                pageSize: 50,
+                pageNumber: 1,
+              },
+            },
+          )
+          .pipe(map((res) => res.data.results)),
+      );
+    } catch (e) {
+      console.error(e);
+    }
+    return [];
+  }
+}
